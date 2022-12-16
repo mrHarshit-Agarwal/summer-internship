a07dcf11e2da5645f94ffccf8aadea552dcdaafd
harshit <harshitag98.ha@gmail.com>
2022-12-16 19:31:05 +0530

    change

--- config/myUrl.js
+++ config/myUrl.js
@@ -1,6 +1,5 @@
 module.exports = myUrl = {
   mongoDB:process.env.MONGO_URI || "mongodb+srv://harshit:harshitdb@cluster0.uzh6tig.mongodb.net/test",
-  // mongoBD:
-  //   "mongodb+srv://Akshar:akshar@2021@cluster0.begp6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  // mongoDB:
   // mongodb+srv://harshit:<password>@cluster0.uzh6tig.mongodb.net/test
 };
